41f001cd0870b523f594e724e12b4d9dca68bba9
Swarnim Sinha <sinhaswarnim07@gmail.com>
2024-06-27 11:39:21 +0000

    fixed the edit gemstones on the edit products modal.

--- pages/Products/jsobjects/JSObject3/JSObject3.js
+++ pages/Products/jsobjects/JSObject3/JSObject3.js
@@ -1,14 +1,18 @@
 export default {
 
-	async myFun2 () {
-		let myPromise = new Promise(function(){
-			get_Gemstoneproduct.run()
-		});
-		myPromise.then(
-			function(){get_gemstonecutedit.run()}
-		);
-
-
+async	myFun2 () {
+get_Gemstoneproduct.run()
+		.then(result=>{
+	get_gemstonecutedit.run()
+	.then(result=>{
+	get_gemstonesizeedit.run()
+		.then(result=>{
+	 get_gemstoneshapeedit.run()
+})
+		
+})
+		
+});
 		//	use async-await or promises
 		//	await storeValue('varName', 'hello world')
 	}
